6a09916d718ac5af22df948563a2422c3fe457e1
Douglas Queiroz <152705113+dev-queiroz@users.noreply.github.com>
2024-08-02 17:07:00 -0300

    "Added emailjs dependency, updated package-lock.json and package.json, removed cors and node-mailjet dependencies, and refactored server.js to use emailService"

--- server.js
+++ server.js
@@ -1,42 +1,17 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const Mailjet = require('node-mailjet');
-const cors = require('cors');
+const sendEmail = require('./emailService');
 require('dotenv').config();
 
 const app = express();
-app.use(cors());
 app.use(bodyParser.json());
 
-const mailjet = Mailjet.apiConnect(
-  process.env.MAILJET_API_KEY,
-  process.env.MAILJET_API_SECRET
-);
-
 app.post('/send-email', async (req, res) => {
-  const { to, subject, text } = req.body;
+  const { email } = req.body;
 
   try {
-    const result = await mailjet
-      .post('send', { version: 'v3.1' })
-      .request({
-        Messages: [
-          {
-            From: {
-              Email: 'dev.queiroz05@gmail.com',
-              Name: 'Douglas Queiroz'
-            },
-            To: [
-              {
-                Email: to
-              }
-            ],
-            Subject: subject,
-            TextPart: text
-          }
-        ]
-      });
-    res.status(200).json(result.body);
+    await sendEmail(email);
+    res.status(200).json({ message: 'Email sent successfully!' });
   } catch (error) {
     res.status(500).json({ error: error.message });
   }
