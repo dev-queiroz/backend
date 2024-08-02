3b5b6fb7b990edab2beee29621c0d28535ba56a4
Douglas Queiroz <152705113+dev-queiroz@users.noreply.github.com>
2024-08-02 14:20:23 -0300

    "Deleted multiple files and folders, including controllers, models, routes, services, and views, and updated package.json"

--- services/supabase.js
+++
@@ -1,9 +0,0 @@
-require("dotenv").config();
-const { createClient } = require("@supabase/supabase-js");
-
-const supabaseUrl = process.env.SUPABASE_URL;
-const supabaseAnonKey = process.env.SUPABASE_ANON_KEY;
-
-const supabase = createClient(supabaseUrl, supabaseAnonKey);
-
-module.exports = supabase;
